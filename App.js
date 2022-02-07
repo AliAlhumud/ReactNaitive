13a527ce66f45778f6b7a8472322b0bb223d26f2
Ali Alhumud <Ali.alhumud1@gmail.com>
2022-02-07 18:49:51 +0300

    Add files via upload
    User data

--- App.js
+++ App.js
@@ -1,46 +1,57 @@
 import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 
 
 export default function App() {
- 
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [couseGoals, setCourseGoals] = useState([]);
+  const goalInputHandeler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
+  };
   return (
-    <View style= {{padding: 250, flexDirection: 'row-reverse', justifyContent: 'center'}}>
+    <View style={styles.container}>
 
-<View style={{
-  backgroundColor: 'red',
-  width: 100,
-  height: 100,
-  justifyContent: 'center',
-  alignItems: 'center'}}>
+      <View style={styles.screen}>
+        <TextInput placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandeler}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
+        <View>
 
-  <Text>1</Text>
-</View>
-  <View style={{
-  backgroundColor: 'blue',
-  width: 100,
-  height: 100,
-  justifyContent: 'center',
-  alignItems: 'center'}}>
-  <Text>2</Text>
-    
-  </View>
-  <View style={{
-  backgroundColor: 'green',
-  width: 100,
-  height: 100,
-  justifyContent: 'center',
-  alignItems: 'center'}}>
-  <Text>3</Text>
-    
-  </View>
+          {couseGoals.map((goal) => <Text>{goal}</Text>)}
+        </View>
+      </View>
+    </View>
 
-
-
-
-</View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 50
+  },
+  screen: {
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+
+  },
+
+
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+
+
+});
