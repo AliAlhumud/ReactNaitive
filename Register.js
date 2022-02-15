b7a6c24c12c9b514d125b2348ba9b845b108051f
Ali Alhumud <Ali.alhumud1@gmail.com>
2022-02-15 11:34:35 +0300

    Create Register.js
    Register user email and password to send it to firebase,  required additional firebase setup, configuration file check https://firebase.google.com/docs/auth/web/start

---
+++ Register.js
@@ -0,0 +1,30 @@
+export default function App() {
+
+  const [isSignedIn,setIsSignedIn] = useState(false);
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const RegisterUser =() =>{
+createUserWithEmailAndPassword(authentication, email, password)
+.then((re)=>{
+console.log(re);
+setIsSignedIn(true)
+})
+.catch((re)=>{
+  console.log(re);
+  
+  })
+  }
+  return (
+   <View>
+ <TextInput placeholder='Email'value={email} onChangeText={text=>setEmail(text)}/>
+ <TextInput placeholder='password'value={password} secureTextEntry={true} onChangeText={text=>setPassword(text)}/>
+
+ <Button title='Register' onPress={RegisterUser}/>
+
+ <Button title='Sign in' />
+ 
+
+
+   </View>
+  );
+}
